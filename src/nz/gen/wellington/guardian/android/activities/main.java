be5238f69519b495e8df2b6db2a2524d286996c7
Tony McCrae <tony@Morden.local>
2010-06-07 13:01:54 +0100

    Remove main screen hack. Too slow.

--- src/nz/gen/wellington/guardian/android/activities/main.java
+++ src/nz/gen/wellington/guardian/android/activities/main.java
@@ -27,22 +27,11 @@ public class main extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-                
-    	List<Article> articles = ArticleDAOFactory.getDao(this).getTopStories();
-    	populateNewsitemList(articles);
+   
 	}
 	
 	
-	private void populateNewsitemList(List<Article> articles) {
-		if (articles != null) {			
-			List<Article> newsitems = articles;	
-			ListView listView = (ListView) findViewById(R.id.ArticlesListView);    		   
-			adapter = new ListArticleAdapter(this, ArticleImageDecorator.decorateNewsitemsWithThumbnails(newsitems, this));		   
-			listView.setAdapter(adapter);
-		} else {
-			Toast.makeText(this, "Could not load section articles", Toast.LENGTH_SHORT).show();
-		}
-	}
+	
 	
 	
 	public boolean onCreateOptionsMenu(Menu menu) {
