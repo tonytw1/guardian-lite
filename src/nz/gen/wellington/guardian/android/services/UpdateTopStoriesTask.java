b8eb6cfb7ff48b149c8363c467a885ebbadbda0b
Tony McCrae <mccraetony@googlemail.com>
2010-06-10 13:51:27 +0100

    TreeSet abuse to sort section articles by publication date on main screen.

--- src/nz/gen/wellington/guardian/android/services/UpdateTopStoriesTask.java
+++ src/nz/gen/wellington/guardian/android/services/UpdateTopStoriesTask.java
@@ -1,12 +1,18 @@
 package nz.gen.wellington.guardian.android.services;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 import nz.gen.wellington.guardian.android.api.ArticleDAO;
 import nz.gen.wellington.guardian.android.model.Article;
 import nz.gen.wellington.guardian.android.model.ContentUpdateReport;
 import nz.gen.wellington.guardian.android.model.Section;
+
+import org.joda.time.DateTime;
+
 import android.content.Context;
 import android.util.Log;
 
@@ -21,28 +27,24 @@ public class UpdateTopStoriesTask implements ContentUpdateTaskRunnable {
 
 	@Override
 	public void run() {
-		
 		Log.i(TAG, "Updating top stories");
-		List<Article> topStories = new ArrayList<Article>();
+		SortedMap<DateTime, Article> topStories = new TreeMap<DateTime, Article>();
 		List<Section> sections = articleDAO.getSections();
 		if (sections != null) {
 			for (Section section : sections) {
 				List<Article> sectionItems = articleDAO.getSectionItems(section);
 				if (sectionItems != null) {
 					Log.i(TAG, "Adding story: " + sectionItems.get(0));
-					topStories.add(sectionItems.get(0));
+					topStories.put(sectionItems.get(0).getPubDate(), sectionItems.get(0));
 				}
 			}
 		}
 		
-		// TODO sort by date and limit to 10 say
-		if (topStories.size() > 10) {
-			topStories = topStories.subList(0, 10);
-		}
-		
 		Log.i(TAG, "Saving " + topStories.size() + " top stories");
-		articleDAO.saveTopStories(new ArrayList<Article>(topStories));
-		Log.i(TAG, "Done");
+		ArrayList<Article> results = new ArrayList<Article>(topStories.values());
+		Collections.reverse(results);
+		articleDAO.saveTopStories(results);
+		Log.i(TAG, "Done");		
 	}
 
 	@Override
