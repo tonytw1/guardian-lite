bcc6bebc54b31cfac85c3783f8577dc759052a10
Tony McCrae <tony@morden.int.gnl>
2010-06-14 18:02:15 +0100

    Grouping articles into sections groups.

--- src/nz/gen/wellington/guardian/android/services/UpdateTopStoriesTask.java
+++ src/nz/gen/wellington/guardian/android/services/UpdateTopStoriesTask.java
@@ -1,5 +1,6 @@
 package nz.gen.wellington.guardian.android.services;
 
+import java.security.acl.LastOwnerException;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
@@ -35,25 +36,57 @@ public class UpdateTopStoriesTask implements ContentUpdateTaskRunnable {
 		if (sections != null) {
 			for (Section section : sections) {
 				List<Article> sectionItems = articleDAO.getSectionItems(section);
-				if (sectionItems != null) {
-					// TODO correct sublisting
-					topStories.put(sectionItems.get(0).getPubDate(), sectionItems.get(0));
-					topStories.put(sectionItems.get(1).getPubDate(), sectionItems.get(1));
-				}
+				putLatestThreeStoriesFromSectionOntoList(topStories, sectionItems);
 			}
 		}
 		
-		Log.i(TAG, "Saving " + topStories.size() + " top stories");
+		// List now contains 3 items from each section in date order.
+		LinkedList<Article> results = new LinkedList<Article>();
+		while (!topStories.isEmpty()) {
+			Article latest = topStories.get(topStories.firstKey());
+			Log.d(TAG, "Latest article has section: " + latest.getSection().getName());
+			results.add(latest);
+			
+			addArticlesForSection(topStories, results, latest.getSection());
+			
+		}
 				
-		LinkedList<Article> results = new LinkedList<Article>(topStories.values());
-		Collections.reverse(results);
-		//if (results.size() > 10) {
-		//	results = new LinkedList<Article>(results.subList(0, 10));
-		//}
+		Log.i(TAG, "Saving " + topStories.size() + " top stories");
+		Collections.reverse(results);		
 		articleDAO.saveTopStories(results);
 		Log.i(TAG, "Done");
 	}
 
+	
+	
+	private void addArticlesForSection(SortedMap<DateTime, Article> topStories,
+			LinkedList<Article> results, Section section) {
+		
+		for (Article article : new LinkedList<Article>(topStories.values())) {
+			if (article.getSection().getId().equals(section.getId())) {
+				results.add(article);
+				topStories.remove(article.getPubDate());
+			}
+		}
+
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void putLatestThreeStoriesFromSectionOntoList(SortedMap<DateTime, Article> topStories, List<Article> sectionItems) {
+		if (sectionItems == null) {
+			return;
+		}
+		final int numberToAdd = (sectionItems.size() < 3) ? sectionItems.size() : 3; 
+		for (int i = 0; i < numberToAdd; i++) {
+			Article article = sectionItems.get(i);
+			if (article.getSection() != null) {
+				topStories.put(article.getPubDate(), article);
+			}
+		}
+	}
+
+	
 	@Override
 	public void setReport(ContentUpdateReport report) {			
 	}
