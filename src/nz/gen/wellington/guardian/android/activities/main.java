68ae5b35d09bf31cc8f93a665f10040ad7503f1f
Tony McCrae <tony@Morden.local>
2010-07-12 09:22:35 +0100

    About dialog wording.

--- src/nz/gen/wellington/guardian/android/activities/main.java
+++ src/nz/gen/wellington/guardian/android/activities/main.java
@@ -9,6 +9,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.Window;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class main extends ArticleListActivity {
 		
@@ -78,11 +79,19 @@ public class main extends ArticleListActivity {
 		dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		dialog.setContentView(R.layout.about_dialog);
 		dialog.show();
+		
 		ImageView heading = (ImageView) dialog.findViewById(R.id.KingsPlace);
 		heading.setImageResource(R.drawable.kingsplace);
 		
+		TextView description = (TextView) dialog.findViewById(R.id.Description);		
+		description.setText("This unofficial application was developed by Tony McCrae of Eel Pie Consulting Limited.\n\n" +
+				"Articles are retreived from the Guardian's RSS feeds. Tag information is supplied by the Guardian Content API.\n\n" +
+				"For more information see:\nhttp://eelpieconsulting.co.uk/guardianlite\n\n" +
+				"Application © 2010 Eel Pie Consulting Limited\n\n" +
+				"Content © 2010 Guardian News and Media Limited");
+				
 		ImageView image = (ImageView) dialog.findViewById(R.id.GuardianLogo);
-		image.setImageResource(R.drawable.poweredbyguardian);		
+		image.setImageResource(R.drawable.poweredbyguardian);
 		dialog.setTitle(null);
 	}
 	
