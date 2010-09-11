892b2c64bf1347398726355ee4e393ac6d2fab78
Tony McCrae <tony@Morden.local>
2010-09-11 13:45:40 +0100

    Adding missing file.

---
+++ src/nz/gen/wellington/guardian/android/usersettings/PreferencesDAO.java
@@ -0,0 +1,25 @@
+package nz.gen.wellington.guardian.android.usersettings;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+public class PreferencesDAO {
+
+	private SharedPreferences prefs;
+
+	public PreferencesDAO(Context context) {
+		prefs =  PreferenceManager.getDefaultSharedPreferences(context);
+	}
+
+	public int getPageSizePreference() {
+		final String pageSizeString = prefs.getString("pageSize", "10");
+		int pageSize = Integer.parseInt(pageSizeString);
+		return pageSize;
+	}
+
+	public boolean getLargePicturesPreference() {
+		return (Boolean) prefs.getBoolean("largeImages", false);
+	}
+	
+}
