5a61d79f3e9821e9565a2b0815d4e831706df61b
Tony McCrae <tony@Morden.local>
2010-11-14 22:28:14 +0000

    Pulling an abstract class to deduplicate the widget click through activities.

--- src/nz/gen/wellington/guardian/android/activities/mainwidget.java
+++ src/nz/gen/wellington/guardian/android/activities/mainwidget.java
@@ -1,47 +1,11 @@
 package nz.gen.wellington.guardian.android.activities;
 
-import nz.gen.wellington.guardian.android.api.ArticleDAO;
-import nz.gen.wellington.guardian.android.api.ArticleDAOFactory;
-import nz.gen.wellington.guardian.android.api.ContentFetchType;
-import nz.gen.wellington.guardian.android.model.Article;
-import nz.gen.wellington.guardian.android.model.ArticleBundle;
-import nz.gen.wellington.guardian.android.model.ArticleSet;
 import nz.gen.wellington.guardian.android.model.TopStoriesArticleSet;
-import android.app.Activity;
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Bundle;
 
-public class mainwidget extends Activity {
-
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-	}
-
-	protected void onResume() {
-		super.onResume();
-		Uri dataUri = this.getIntent().getData();
-		if (dataUri != null) {
-			final String articleId = dataUri.getLastPathSegment();
-
-			ArticleSet articleSet = new TopStoriesArticleSet();
-			ArticleDAO articleDAO = ArticleDAOFactory.getDao(this.getApplicationContext());
-			ArticleBundle bundle = articleDAO.getArticleSetArticles(articleSet, ContentFetchType.LOCAL_ONLY);
-			if (bundle != null) {
-				for (Article article : bundle.getArticles()) {
-					if (article.getId().equals(articleId)) {
-						Intent intent = new Intent(this, article.class);
-						intent.putExtra("article", article);
-						this.startActivity(intent);
-						return;
-					}
-				}
-			}
-			
-		}
-		Intent intent = new Intent(this, main.class);
-		this.startActivity(intent);
+public class mainwidget extends WidgetClickthroughActivity {
+	
+	protected TopStoriesArticleSet getArticleSet() {
+		return new TopStoriesArticleSet();
 	}
 
 }
