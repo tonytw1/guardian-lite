6c63a64909e21358c212122ecf8cc83606cafe34
Tony McCrae <tony@Morden.local>
2010-11-23 21:23:36 +0000

    Introducing image cache class to remove low level file calls from image dao.

--- src/nz/gen/wellington/guardian/android/api/ImageDAO.java
+++ src/nz/gen/wellington/guardian/android/api/ImageDAO.java
@@ -1,12 +1,6 @@
 package nz.gen.wellington.guardian.android.api;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-
-import nz.gen.wellington.guardian.android.api.caching.FileService;
+import nz.gen.wellington.guardian.android.api.caching.FileBasedImageCache;
 import nz.gen.wellington.guardian.android.network.HttpFetcher;
 import android.content.Context;
 import android.graphics.Bitmap;
@@ -17,60 +11,41 @@ public class ImageDAO {
 	//private static final String TAG = "ImageDAO";
 	
 	private HttpFetcher httpFetcher;
-	private Context context;
+	private FileBasedImageCache imageCache;
 		
 	public ImageDAO(Context context) {
+		this.imageCache = new FileBasedImageCache(context);
 		httpFetcher = new HttpFetcher(context);
-		this.context = context;
 	}
 
-	// TODO shouuld be a seperate cache, so that only caches talk to the file system directly.
 	public boolean isAvailableLocally(String url) {
-		return FileService.existsLocally(context, url);
+		return imageCache.isAvailableLocally(url);
 	}
 		
 	public Bitmap getImage(String url) {
-		try {
-			InputStream fis = FileService.getFileInputStream(context, url);
-			ObjectInputStream in = new ObjectInputStream(fis);
-			byte[] image = (byte[]) in.readObject();
-			in.close();
-			
-			return decodeImage(image);
-			
-		} catch (IOException ex) {
-			//Log.e(TAG, "IO Exception while writing article set: " + url + ex.getMessage());
-		} catch (ClassNotFoundException ex) {
-			//Log.e(TAG, "Exception while writing article set: " + url + ex.getMessage());
+		if (isAvailableLocally(url)) {						
+			byte[] cachedImage = imageCache.getCachedImage(url);
+			if (cachedImage != null) {
+				return decodeImage(cachedImage);
+			}
 		}
 		return null;
 	}
-
-
+	
+	// TODO this method needs to fall in behind getImage.
+	// There is probably caching code in the frontend classes which can be pulled into this DAO.
 	public Bitmap fetchLiveImage(String url) {
-		byte[] image = httpFetcher.httpFetchStream(url);
-		
+		byte[] image = httpFetcher.httpFetchStream(url);		
 		if (image == null) {
 			//Log.i(TAG, "Could not fetch image: " + url);
 			return null;
 		}
-		
+
 		//Log.i(TAG, "Writing image to disk: " + url);
-		ObjectOutputStream out = null;
-		try {		
-			FileOutputStream fos = FileService.getFileOutputStream(context, url);
-			out = new ObjectOutputStream(fos);
-			out.writeObject(image);
-			out.close();
-			
-		} catch (IOException ex) {
-			ex.printStackTrace();
-		}	
-				
+		imageCache.saveImageToFile(url, image);				
 		return decodeImage(image);
 	}
-
-	
+		
 	private Bitmap decodeImage(byte[] image) {
 		Bitmap bitmap = BitmapFactory.decodeByteArray(image, 0, image.length);
 		return bitmap;
---
+++ src/nz/gen/wellington/guardian/android/api/caching/FileBasedImageCache.java
@@ -0,0 +1,52 @@
+package nz.gen.wellington.guardian.android.api.caching;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import android.content.Context;
+
+public class FileBasedImageCache {
+
+	private Context context;
+
+	public FileBasedImageCache(Context context) {
+		this.context = context;
+	}
+
+	public boolean isAvailableLocally(String url) {
+		return FileService.existsLocally(context, url);
+	}
+	
+	public byte[] getCachedImage(String url) {
+		try {
+			InputStream fis = FileService.getFileInputStream(context, url);
+			ObjectInputStream in = new ObjectInputStream(fis);
+			byte[] image = (byte[]) in.readObject();
+			in.close();
+			return image;
+			
+		} catch (IOException ex) {
+			//Log.e(TAG, "IO Exception while writing article set: " + url + ex.getMessage());
+		} catch (ClassNotFoundException ex) {
+			//Log.e(TAG, "Exception while writing article set: " + url + ex.getMessage());
+		}
+		return null;
+	}
+	
+	public void saveImageToFile(String url, byte[] image) {
+		ObjectOutputStream out = null;
+		try {		
+			FileOutputStream fos = FileService.getFileOutputStream(context, url);
+			out = new ObjectOutputStream(fos);
+			out.writeObject(image);
+			out.close();
+			
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+}
