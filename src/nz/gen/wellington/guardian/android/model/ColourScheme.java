3ef3453f739e7e690e8f60abce1f88ce04564b38
Tony McCrae <tony@Morden.local>
2010-12-30 20:46:11 +0000

    Set background colour to android default (which is not quite black).

--- src/nz/gen/wellington/guardian/android/model/ColourScheme.java
+++ src/nz/gen/wellington/guardian/android/model/ColourScheme.java
@@ -5,7 +5,7 @@ import android.graphics.Color;
 public class ColourScheme {
 	
 	public static final int BODYTEXT = Color.LTGRAY;
-	public static final int BACKGROUND = Color.BLACK;
+	public static final int BACKGROUND = Color.parseColor("#FF191919");
 	public static final int HEADLINE = Color.WHITE;
 	
 	public static final int AVAILABLE_TAG = Color.WHITE;
