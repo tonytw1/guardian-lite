6b4d0ad6d62e0aebd91803ef67ee5fd3856b46bc
Tony McCrae <tony@Morden.local>
2011-01-28 19:25:38 +0000

    Cleaning up colour definitions by naming as constants. Going static with the map might save some GC churn as well.

--- src/nz/gen/wellington/guardian/android/model/SectionColourMap.java
+++ src/nz/gen/wellington/guardian/android/model/SectionColourMap.java
@@ -5,29 +5,44 @@ import java.util.Map;
 
 public class SectionColourMap {
 
-	public static String getColourForSection(String sectionId) {
-		Map<String, String> sectionColours = new HashMap<String, String>();
-				
-		sectionColours.put("business", "#4A64D9");
-		sectionColours.put("commentisfree", "#0061A6");
-		sectionColours.put("culture", "#D1008B");
-		sectionColours.put("education", "#801100");
-		sectionColours.put("environment", "#7BBB00");
-		sectionColours.put("football", "#006000");
-		sectionColours.put("lifeandstyle", "#FFC202");
-		sectionColours.put("media", "#801100");
-		sectionColours.put("money", "#8F1AB6");
-		sectionColours.put("politics", "#801100");
-		sectionColours.put("science", "#801100");
-		sectionColours.put("society", "#801100");
-		sectionColours.put("sport", "#008000");
-		sectionColours.put("travel", "#65C5FB");	
-		
+	private static final String TRAVEL_BLUE = "#65C5FB";
+	private static final String SPORT_GREEN = "#008000";
+	private static final String MONEY_PURPLE = "#8F1AB6";
+	private static final String LIFE_AND_STYLE_YELLOW = "#FFC202";
+	private static final String FOOTBALL_GREEN = "#006000";
+	private static final String CULTURE_PINK = "#D1008B";
+	private static final String ENVIRONMENT_GREEN = "#7BBB00";
+	private static final String GUARDIAN_RED = "#801100";
+	private static final String CIF_BLUE = "#0061A6";
+	private static final String BUSINESS_BLUE = "#4A64D9";
+	private static final String DEFAULT_SECTION_COLOUR = "#D61D00";
+	
+	private static Map<String, String> sectionColours = buildSectionColourMap();
+
+	public static String getColourForSection(String sectionId) {	
 		if (sectionColours.containsKey(sectionId)) {
 			return sectionColours.get(sectionId);
 		}
-		
-		return "#D61D00";
+		return DEFAULT_SECTION_COLOUR;
+	}
+
+	private static Map<String, String> buildSectionColourMap() {
+		Map<String, String> sectionColours = new HashMap<String, String>();
+		sectionColours.put("business", BUSINESS_BLUE);
+		sectionColours.put("commentisfree", CIF_BLUE);
+		sectionColours.put("culture", CULTURE_PINK);
+		sectionColours.put("education", GUARDIAN_RED);
+		sectionColours.put("environment", ENVIRONMENT_GREEN);
+		sectionColours.put("football", FOOTBALL_GREEN);
+		sectionColours.put("lifeandstyle", LIFE_AND_STYLE_YELLOW);
+		sectionColours.put("media", GUARDIAN_RED);
+		sectionColours.put("money", MONEY_PURPLE);
+		sectionColours.put("politics", GUARDIAN_RED);
+		sectionColours.put("science", GUARDIAN_RED);
+		sectionColours.put("society", GUARDIAN_RED);
+		sectionColours.put("sport", SPORT_GREEN);
+		sectionColours.put("travel", TRAVEL_BLUE);
+		return sectionColours;
 	}
 
 }
