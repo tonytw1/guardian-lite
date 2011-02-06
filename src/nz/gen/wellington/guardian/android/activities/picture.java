bf2da16d679fc05c15162a54a4596ee951b846a7
Tony McCrae <tony@Morden.local>
2011-02-06 17:21:07 +0000

    Picture view main image load using the loader/handler pattern.

--- src/nz/gen/wellington/guardian/android/activities/picture.java
+++ src/nz/gen/wellington/guardian/android/activities/picture.java
@@ -4,7 +4,10 @@ import nz.gen.wellington.guardian.android.R;
 import nz.gen.wellington.guardian.android.api.ImageDAO;
 import nz.gen.wellington.guardian.android.factories.SingletonFactory;
 import nz.gen.wellington.guardian.android.model.Picture;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.TypedValue;
 import android.view.View;
 import android.view.Window;
@@ -15,8 +18,12 @@ import android.widget.ImageView.ScaleType;
 
 public class picture extends AbstractFontResizingActivity {
 
+	private MainImageLoader mainImageLoader;
+	private MainImageUpdateHandler mainImageUpdateHandler;
+
 	private ImageDAO imageDAO;
 	private Picture picture;
+	private Bitmap image;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -24,19 +31,30 @@ public class picture extends AbstractFontResizingActivity {
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.picture);
 		
-		imageDAO = SingletonFactory.getImageDao(this.getApplicationContext());
-		
 		this.picture = (Picture) this.getIntent().getExtras().get("picture");		
 		if (picture != null) {
-			ImageView imageView = (ImageView) findViewById(R.id.ArticleImage);
-			imageView.setVisibility(View.VISIBLE);
-			imageView.setScaleType(ScaleType.FIT_CENTER);
-			imageView.setImageBitmap(imageDAO.getImage(picture.getFile()));	// TODO safe the image load			
-			populateCaption(picture.getCaption());
-
+			populateMainImage();
+			imageDAO = SingletonFactory.getImageDao(this.getApplicationContext());
+			mainImageUpdateHandler = new MainImageUpdateHandler();
+    	
+			final String mainImageUrl = picture.getFile();
+			if (mainImageUrl != null) {
+				mainImageLoader = new MainImageLoader(imageDAO, picture.getFile());
+				Thread loader = new Thread(mainImageLoader);
+				loader.start();
+			}
+			
 		} else {
         	Toast.makeText(this, "Could not load picture", Toast.LENGTH_SHORT).show();
 		}
+	}
+
+	protected void populateMainImage() {
+		ImageView imageView = (ImageView) findViewById(R.id.ArticleImage);
+		imageView.setVisibility(View.VISIBLE);
+		imageView.setScaleType(ScaleType.FIT_CENTER);
+		imageView.setImageBitmap(image);		
+		populateCaption(picture.getCaption());
 	}
 	
 	@Override
@@ -63,4 +81,46 @@ public class picture extends AbstractFontResizingActivity {
 			captionView.setText(caption);
 		}
 	}
+	
+	
+	class MainImageLoader implements Runnable {		
+
+		private ImageDAO imageDAO;
+		private String mainImageUrl;
+		
+		public MainImageLoader(ImageDAO imageDAO, String mainImageUrl) {
+			this.imageDAO = imageDAO;
+			this.mainImageUrl = mainImageUrl;
+		}
+		
+		@Override
+		public void run() {
+			image = imageDAO.getImage(mainImageUrl);
+			if (image != null) {
+				sendMainImageAvailableMessage(mainImageUrl);
+			}
+			return;
+		}
+
+		private void sendMainImageAvailableMessage(String mainImageUrl) {
+			Message msg = new Message();
+			msg.what = MainImageUpdateHandler.MAIN_IMAGE_AVAILABLE;
+			mainImageUpdateHandler.sendMessage(msg);
+		}		
+	}
+	
+	class MainImageUpdateHandler extends Handler {
+		
+		private static final int MAIN_IMAGE_AVAILABLE = 1;
+
+		public void handleMessage(Message msg) {
+			super.handleMessage(msg);
+			
+			switch (msg.what) {	   
+			    case MAIN_IMAGE_AVAILABLE:
+			    populateMainImage();
+			}
+		}
+	}
+	
 }
